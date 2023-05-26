30342f28986a99ad3429be3e4a2b177e8448d5bd
eltonvan <eltonvansoylent@gmail.com>
2023-05-26 11:05:41 +0200

    final version

--- icalconvertor.py
+++ icalconvertor.py
@@ -8,29 +8,30 @@ def pdf_to_ics(pdf_file, ics_file):
     cal = Calendar()
 
     for page_num in range(num_pages):
-        print(page_num)
+        
         page = pdf.getPage(page_num)
-        content = page.extract_text()
+        content = page.extractText()
 
         # Assuming the table has two columns separated by a tab character
         rows = content.strip().split('\n')
-
+        
         for row in rows:
             data = row.split('\t')
+            print(data)
 
-            # Assuming the table has at least two columns: date and description
-            if len(data) >= 2:
-                date = data[0].strip()
-                description = data[1].strip()
+    #         # Assuming the table has at least two columns: date and description
+    #         if len(data) >= 2:
+    #             date = data[0].strip()
+    #             description = data[1].strip()
 
-                event = Event()
-                event.add('summary', description)
-                event.add('dtstart', date)
-                event.add('dtend', date)
-                cal.add_component(event)
+    #             event = Event()
+    #             event.add('summary', description)
+    #             event.add('dtstart', date)
+    #             event.add('dtend', date)
+    #             cal.add_component(event)
 
-    with open(ics_file, 'wb') as f:
-        f.write(cal.to_ical())
+    # with open(ics_file, 'wb') as f:
+    #     f.write(cal.to_ical())
 
 
 pdf_file_path = 'kinder.pdf'
